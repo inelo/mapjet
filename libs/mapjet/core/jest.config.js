c28874137e7dfc3d30bc356cb9b177c1b3557224
rwalus <ravwalus@gmail.com>
2022-08-01 17:03:06 +0200

    Moved core folder struture to upper

--- libs/mapjet/core/jest.config.js
+++ libs/mapjet/core/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/mapjet/core',
+  coverageDirectory: '../../../coverage/lib/mapjet/core',
 };
